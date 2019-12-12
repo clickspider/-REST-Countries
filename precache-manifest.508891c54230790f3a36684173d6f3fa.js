7d23249f1877ba9ec3b591d023aa85b8e852f958
clickspider <danielfrey101@gmail.com>
2019-12-12 20:08:02 +0200

    build(upload project): final dist

---
+++ precache-manifest.508891c54230790f3a36684173d6f3fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75460758abeb09fc0f0",
+    "url": "/css/app.fb9f81c7.css"
+  },
+  {
+    "revision": "eb211eed665350dcd429",
+    "url": "/css/chunk-vendors.5f51e979.css"
+  },
+  {
+    "revision": "5371f2b020deff5611322cb4430ddc90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "263c4490f1ccba033c2d",
+    "url": "/js/about.781b8cca.js"
+  },
+  {
+    "revision": "a75460758abeb09fc0f0",
+    "url": "/js/app.fe1b3d74.js"
+  },
+  {
+    "revision": "eb211eed665350dcd429",
+    "url": "/js/chunk-vendors.948477ac.js"
+  },
+  {
+    "revision": "530ae3c637d1367a215c5978d9f3d7ae",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
